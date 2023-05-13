008cfd18db9f31ef3290ab3222daea68d2b7dd68
mhshoeb <challobin279@gmail.com>
2023-05-13 13:29:57 +0600

    cityclick&back

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -75,5 +75,17 @@ public class MainActivityTest {
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click()); //Check the content on the list - no content in this case
         Espresso.pressBack(); //Back button
     }
+    @Test
+   public void test3(){
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton")); //Type a city name
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
 
-}
+        onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click()); //Check the content on the list - no content in this case
+        onView(withId(R.id.button)).perform(click());
+
+    }
+
+    }
+
+
---
+++ app/src/androidTest/java/com/example/citylist/show_activityTest.java
@@ -0,0 +1,9 @@
+package com.example.citylist;
+
+import static org.junit.Assert.*;
+
+import org.junit.runner.RunWith;
+
+public class show_activityTest {
+
+}
